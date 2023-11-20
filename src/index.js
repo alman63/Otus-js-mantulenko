6c18ef627e252ac3b301be599ade7b54f86ee352
unknown <mantulenko83@mail.ru>
2023-11-20 19:13:29 +0400

    progectReadyInWork

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+(async () => {
+    const readStream = fs.createReadStream(__dirname + '/data/input.txt', {
+        encoding: 'utf8',
+    });
+
+    let data = '';
+
+    // Через async/await
+    for await (const chunk of readStream) {
+        data += chunk;
+    }
+
+    console.log(data);
+})();
