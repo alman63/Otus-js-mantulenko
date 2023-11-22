803807047fcf1fdc02ab0af01a7f380e71f0e24c
unknown <mantulenko83@mail.ru>
2023-11-22 10:01:27 +0400

    Ready

--- src/index.js
+++ src/index.js
@@ -1,16 +1,74 @@
 const fs = require('fs');
+const { chunk } = require('lodash');
+const { Transform } = require('stream');
 
 (async () => {
     const readStream = fs.createReadStream(__dirname + '/data/input.txt', {
         encoding: 'utf8',
     });
+    const writeStream = fs.createWriteStream(__dirname + '/data/output.txt', {
+        encoding: 'utf8',
+    });
 
-    let data = '';
+    const filterTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(
+                null,
+                JSON.stringify(
+                    chunk
+                        .toString()
+                        .replace(/[^a-z ]/g, '')
+                        .replace(/\s{2}/g, ' ')
+                        .split(' ')
+                        .filter((el) => el !== '')
+                        .sort((a, b) => {
+                            if (a.toLowerCase() < b.toLowerCase()) {
+                                return -1;
+                            }
+                            if (a.toLowerCase() > b.toLowerCase()) {
+                                return 1;
+                            }
+                            return 0;
+                        })
+                )
+            );
+        },
+    });
 
-    // Через async/await
-    for await (const chunk of readStream) {
-        data += chunk;
-    }
+    const getObjTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkObj = {};
+            const getObj = (chunk) => {
+                const chunkArr = JSON.parse(chunk);
+                for (const item of chunkArr) {
+                    if (chunkObj.hasOwnProperty(item)) {
+                        chunkObj[`${item}`] += 1;
+                    } else {
+                        chunkObj[`${item}`] = 1;
+                    }
+                }
+                return chunkObj;
+            };
+            callback(null, JSON.stringify(getObj(chunk)));
+        },
+    });
+    const getResulArrTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkResultArr = [];
+            const getArr = (chunk) => {
+                const chunkObj = JSON.parse(chunk);
+                for (prop in chunkObj) {
+                    chunkResultArr.push(chunkObj[`${prop}`]);
+                }
+                return chunkResultArr;
+            };
+            callback(null, JSON.stringify(getArr(chunk)));
+        },
+    });
 
-    console.log(data);
+    readStream
+        .pipe(filterTransform)
+        .pipe(getObjTransform)
+        .pipe(getResulArrTransform)
+        .pipe(writeStream);
 })();
